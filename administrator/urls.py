c92561607eca339147d60d4fff60e7ed79ebe027
Cortotrobo <corto.bouviolle@laposte.net>
2022-12-15 08:51:33 +0100

    corto

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from administrator.views import index
+from administrator.views import index, addestablishment, adddepartment, addlink
 
 app_name = 'administrator'
 
 urlpatterns = [
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('add_establishment/', addestablishment, name="addestablishment"),
+    path('add_department/', adddepartment, name="adddepartment"),
+    path('add_link/', addlink, name="addlink")
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template.loader import get_template
 
-from main.models import UsefulLink
+from main.models import *
 from main.views import isAdmin
 
 # Create your views here.
@@ -10,11 +10,62 @@ def index(request):
     admin = isAdmin(user)
 
     if admin == None:
-        return
+        return redirect("accounts:login")
 
     establishments = admin.establishments
 
     return render(request, "a_index.html", context = {
+            "user": user,
+            "establishments" : establishments,
+        }
+    )
+
+
+def addestablishment(request):
+    user = request.user
+    admin = isAdmin(user)
+
+    if admin == None:
+        return
+
+    establishments = admin.establishments
+
+    departments = Department.objects.all()
+
+    return render(request, "addestablishment.html", context = {
+            "user": user,
+            "establishments" : establishments,
+            "departments": departments
+        }
+    )
+
+def adddepartment(request):
+    user = request.user
+    admin = isAdmin(user)
+
+    if admin == None:
+        return
+
+    establishments = admin.establishments
+    departments = Department.objects.all()
+
+    return render(request, "adddepartment.html", context = {
+            "user": user,
+            "establishments" : establishments,
+            "departments": departments
+        }
+    )
+
+def addlink(request):
+    user = request.user
+    admin = isAdmin(user)
+
+    if admin == None:
+        return
+
+    establishments = admin.establishments
+
+    return render(request, "addlink.html", context = {
             "user": user,
             "establishments" : establishments,
         }
