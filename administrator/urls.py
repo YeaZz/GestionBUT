44736fe09a3abee94e30e5d9486df7e5e8eff00c
Noa <63366414+noa-cvlct@users.noreply.github.com>
2023-03-05 17:44:17 +0100

    Better url

--- administrator/urls.py
+++ administrator/urls.py
@@ -10,44 +10,34 @@ urlpatterns = [
     # http://domain/administrator/createEstablishment/ (no template)
     path('createEstablishment/', createEstablishment, name="createEstablishment"),
 
-    # http://domain/administrator/1/
-    path('<establishment_id>/', establishment, name="establishment"),
-
-    # http://domain/administrator/1/createDepartment/ (no template)
-    path('<establishment_id>/createDepartment/', createDepartment, name="createDepartment"),
-
-    #http://domain/administrator/1/1/
-    path('<establishment_id>/<department_id>/', department, name="department"),
-
-    # http://domain/administrator/createCompetence/1/1/ (no template)
-    path('createCompetence/<establishment_id>/<department_id>/', createCompetence, name="createCompetence"),
-
-    # http://domain/administrator/editCompetence/1/1/ (no template)
-    path('editCompetence/<establishment_id>/<department_id>/<competence_id>/', editCompetence, name="editCompetence"),
-
-    # http://domain/administrator/deleteCompetence/1/1/ (no template)
-    path('deleteCompetence/<establishment_id>/<department_id>/<competence_id>/', deleteCompetence, name="deleteCompetence"),
+    # http://domain/administrator/establishment/1/
+    path('establishment/<establishment_id>/', establishment, name="establishment"),
+    # http://domain/administrator/establishment/1/createDepartment/ (no template)
+    path('establishment/<establishment_id>/createDepartment/', createDepartment, name="createDepartment"),
+    #http://domain/administrator/establishment/1/department/1/
+    path('establishment/<establishment_id>/department/<department_id>/', department, name="department"),
 
 
+    # http://domain/administrator/establishment/1/department/1/createCompetence/ (no template)
+    path('establishment/<establishment_id>/department/<department_id>/createCompetence/', createCompetence, name="createCompetence"),
+    # http://domain/administrator/establishment/1/department/1/editCompetence/1/ (no template)
+    path('establishment/<establishment_id>/department/<department_id>/editCompetence/<competence_id>/', editCompetence, name="editCompetence"),
+    # http://domain/administrator/establishment/1/department/1/deleteCompetence/1/ (no template)
+    path('establishment/<establishment_id>/department/<department_id>/deleteCompetence/<competence_id>/', deleteCompetence, name="deleteCompetence"),
 
 
-    # http://domain/administrator/createSemester/1/1/ (no template)
-    path('createSemester/<establishment_id>/<department_id>/', createSemester, name="createSemester"),
-
-    # http://domain/administrator/editSemester/1/1/ (no template)
-    path('editSemester/<establishment_id>/<department_id>/<semester_id>/', editSemester, name="editSemester"),
-
-    # http://domain/administrator/deleteSemester/1/1/ (no template)
-    path('deleteSemester/<establishment_id>/<department_id>/<semester_id>/', deleteSemester, name="deleteSemester"),
+    # http://domain/administrator/establishment/1/department/1/createSemester/ (no template)
+    path('establishment/<establishment_id>/department/<department_id>/createSemester/', createSemester, name="createSemester"),
+    # http://domain/administrator/establishment/1/department/1/editSemester/1/ (no template)
+    path('establishment/<establishment_id>/department/<department_id>/editSemester/<semester_id>/', editSemester, name="editSemester"),
+    # http://domain/administrator/establishment/1/department/1/deleteSemester/1/ (no template)
+    path('establishment/<establishment_id>/department/<department_id>/deleteSemester/<semester_id>/', deleteSemester, name="deleteSemester"),
 
 
-
-    # http://domain/administrator/createGroup/1/1/ (no template)
-    path('createGroup/<establishment_id>/<department_id>/', createGroup, name="createGroup"),
-
-    # http://domain/administrator/editGroup/1/1/ (no template)
-    path('editGroup/<establishment_id>/<department_id>/<group_id>/', editGroup, name="editGroup"),
-
-    # http://domain/administrator/deleteGroup/1/1/ (no template)
-    path('deleteGroup/<establishment_id>/<department_id>/<group_id>/', deleteGroup, name="deleteGroup"),
+    # http://domain/administrator/establishment/1/department/1/createGroup (no template)
+    path('establishment/<establishment_id>/department/<department_id>/createGroup', createGroup, name="createGroup"),
+    # http://domain/administrator/establishment/1/department/1/editGroup/1/ (no template)
+    path('establishment/<establishment_id>/department/<department_id>/editGroup/<group_id>/', editGroup, name="editGroup"),
+    # http://domain/administrator/establishment/1/department/1/deleteGroup/1/ (no template)
+    path('establishment/<establishment_id>/department/<department_id>/deleteGroup/<group_id>/', deleteGroup, name="deleteGroup"),
 ]
